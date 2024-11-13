30498e243dd375fd5ecb9c9b477cd3491feb96d8
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2024-11-13 14:31:03 +0900

    ok

---
+++ DirectXGame/EnemyAnswer.cpp
@@ -0,0 +1,49 @@
+﻿#include "EnemyAnswer.h"
+#include <cassert>
+
+void EnemyAnswer::Initialize(Model* model, const Vector3& position)
+{
+
+	model_ = model;
+	// テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("white1x1.png");
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	worldTransform_.translation_ = position;
+}
+
+void EnemyAnswer::Update()
+{
+
+
+
+	// 行列を更新
+	worldTransform_.UpdateMatrix();
+
+	switch (phase_)
+	{
+	default:
+	case Phase::Approach:
+		// 移動
+		/*worldTransform_.translation_.x -=ApproachSpeed;
+		worldTransform_.translation_.y -=ApproachSpeed;*/
+		worldTransform_.translation_.z -=ApproachSpeed;
+		// 既定の位置に到達したら離脱
+		if (worldTransform_.translation_.z < 0.0f) {
+			phase_ = Phase::Leave;
+		}
+		break;
+	case Phase::Leave:
+		// 移動
+		/*worldTransform_.translation_.x -= LeaveSpeed;
+		worldTransform_.translation_.y -= LeaveSpeed;*/
+		worldTransform_.translation_.z -= LeaveSpeed;
+		break;
+	}
+}
+
+void EnemyAnswer::Draw(const ViewProjection& viewProjection)
+{
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
---
+++ DirectXGame/EnemyAnswer.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <WorldTransform.h>
+#include <Model.h>
+class EnemyAnswer
+{
+public:
+	void Initialize(Model* model, const Vector3& position);
+
+	void Update();
+
+	void Draw(const ViewProjection& viewProjection);
+
+	enum class Phase
+	{
+		Approach, // 接近する
+		Leave,    // 離脱する
+	};
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	// フェーズ
+	Phase phase_ = Phase::Approach;
+
+	// 接近フェーズの移動
+	const float ApproachSpeed = 0.1f;
+	// 離脱フェーズの移動
+	const float LeaveSpeed = 0.2f;
+};
